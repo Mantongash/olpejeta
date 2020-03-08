bdd996d8b1a655a8b5d062302b961061dae45fdf
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-03-08 18:52:00 +0300

    Add group routes for category, species, and users

--- olpejeta-api/routes/web.php
+++ olpejeta-api/routes/web.php
@@ -18,10 +18,44 @@ $router->get('/', function () use ($router) {
 
 $router->get('user/{id}', 'ExampleController@show');
 
-$router->group(['prefix'=>'api'], function() use($router){
-    $router->get('/birds', 'BirdController@index');
-    $router->post('birds', 'BirdController@create');
-    $router->get('birds/{id}', 'BirdController@show');
-    $router->put('birds/{id}', 'BirdController@update');
-    $router->delete('birds/{id}', 'BirdController@destroy');
-    });
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/birds',  'ExampleController@show');
+    $router->put('/birds',  'ExampleController@create');
+    $router->update('/birds',  'ExampleController@update');
+    $router->delete('/birds',  'ExampleController@delete'); 
+    
+  });
+
+
+$router->group(['prefix' => 'api/birds'], function () use ($router) {
+    $router->get('/species',  'SpeciesController@show');
+    $router->put('/species',  'SpeciesController@create');
+    $router->update('/species',  'SpeciesController@update');
+    $router->delete('/species',  'SpeciesController@delete'); 
+    
+  });
+
+  $router->group(['prefix' => 'api/birds'], function () use ($router) {
+    $router->get('/categories',  'CategoriesController@show');
+    $router->put('/categories',  'CategoriesController@create');
+    $router->update('/categories',  'CategoriesController@update');
+    $router->delete('/categories',  'CategoriesController@delete'); 
+    
+  });
+
+
+  $router->group(['prefix' => 'api/birds'], function () use ($router) {
+    $router->get('/sightings',  'SightingsController@show');
+    $router->put('/sightings',  'SightingsController@create');
+    $router->update('/sightings',  'SightingsController@update');
+    $router->delete('/sightings',  'SightintgsController@delete'); 
+    
+  });
+
+  $router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users',  'UsersController@show');
+    $router->put('/users',  'UsersController@create');
+    $router->update('/users',  'UsersController@update');
+    $router->delete('/users',  'UsersController@delete'); 
+    
+  });
