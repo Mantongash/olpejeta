95445d36cc7147d70b55668602cde4a3102fe713
Mantongash <you@example.com>
2020-03-09 16:48:13 +0300

    Made changes in species page

--- frontend/olpejeta/src/app/app.module.ts
+++ frontend/olpejeta/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +9,7 @@ import { UsersComponent } from './components/users/users.component';
 import { GuidesComponent } from './components/guides/guides.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { SpeciesComponent } from './components/species/species.component';
-<<<<<<< HEAD
 import { SightingsComponent } from './components/sightings/sightings.component';
-=======
-import { HttpClientModule } from '@angular/common/http';
-import { DataService } from './data.service';
->>>>>>> 73c2c65b0e99bfaa6e97c93a1076f7bc2540f96a
 // import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 
@@ -33,7 +29,7 @@ import { DataService } from './data.service';
     HttpClientModule,
     // Ng2SearchPipeModule
   ],
-  providers: [DataService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
