93d4c190b16dc6ccd0dc6cb5d28822fc2e93fdef
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-03-11 19:34:27 +0300

    Add an authentication controller

--- olpejeta-api/app/Http/Controllers/AuthController.php
+++ olpejeta-api/app/Http/Controllers/AuthController.php
@@ -2,28 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Users;
-
 use Illuminate\Http\Request;
+use  App\Users;
 
 class AuthController extends Controller
 {
-
-    public function register(Request $request){
-        $request->validate([
-            'user_email' => 'required', 
-            'user_password' => 'required'
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'user_name' => 'required|string',
+            'user_email' => 'required|email|unique:users',
+            'user_password' => 'required|confirmed',
         ]);
 
-            $user =new Users;
-            $user->user_email=$request->user_email;
-            $user->user_password=bcrypt($request->user_email);
+        try {
+
+            $user = new Users;
+            $user->user_name = $request->input('user_name');
+            $user->user_email = $request->input('user_email');
+            // $user->user_type = $request->input('user_type');
+            $plainPassword = $request->input('user_password');
+            $user->user_password = app('hash')->make($plainPassword);
+
             $user->save();
 
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'Successfully registered'], 201);
+
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => $e], 409);
+        }
 
     }
 
-    public function login(){
 
-    }
 }
