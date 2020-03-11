090340b22367b489661e437dec3ce5f90d9eb270
Mantongash <you@example.com>
2020-03-11 14:41:11 +0300

    Changed color scheme of the dashboard

--- frontend/olpejeta/src/app/components/species/species.component.ts
+++ frontend/olpejeta/src/app/components/species/species.component.ts
@@ -20,6 +20,10 @@ export class SpeciesComponent implements OnInit {
     });
   }
 
+  details($event){
+    alert("Niaje msee")
+  }
+
   onSelectedFilter(e) {
     this.getFilteredExpenseList();
   }
