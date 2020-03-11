fa29930e1527f04ba2d66d961a6cc19e0ab54b53
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-03-11 19:35:22 +0300

    Add function to show birds for  a specific sighting

--- olpejeta-api/app/Http/Controllers/SightingsController.php
+++ olpejeta-api/app/Http/Controllers/SightingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Sightings;
+use App\Birds;
 
 use Illuminate\Http\Request;
 
@@ -12,6 +13,14 @@ class SightingsController extends Controller
     public function showAllSightings()
     {
         return response()->json(Sightings::all());
+    }
+
+    //show all sightings from user
+    public function showAllBirdsFromSightings($sightings_id)
+    {
+        $sightings = Sightings::find($sightings_id);
+        $birds = $sightings->birds;
+        return response()->json($birds, 200);
     }
 
     public function showOneSighting($id)
